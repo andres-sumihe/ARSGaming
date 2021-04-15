c18a0d2cc19482e2a481a9ac0ed0dc98faff6f49
andres-sumihe <672018136@student.uksw.edu>
2021-04-15 13:31:14 +0900

    Modify Index, add timer and realtime watch

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    
+?>
+<html>
+    <head>
+        <title>JavaScript</title>
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <script>
+
+    
+    
+    </script>
+    <span id='ct5'></span>
+        <div id="stopwatch">
+            00:00:00
+        </div>
+        <div class="card-timer">
+            <ul id="buttons">
+                <li><button onclick="startTimer()">Start</button></li>
+                <li><button onclick="stopTimer()">Stop</button></li>
+
+                <li><button onclick="resetTimer()">Reset</button></li>
+            </ul>
+        </div>
+
+        <script src="main.js"></script>
+    </body>
+</html>
+
