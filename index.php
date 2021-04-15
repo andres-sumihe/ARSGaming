cb4c847af0f5da8100fce0b8db2e8e0f69f1b81f
andres-sumihe <672018136@student.uksw.edu>
2021-04-15 14:12:36 +0900

    Add responsive on mobile

--- index.php
+++ index.php
@@ -10,25 +10,33 @@
 <head>
     <title>ARSGaming</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?version=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 <body>
-<script>
-
-
-
-</script>
-<span id='ct5'></span>
-    <div id="stopwatch">
-        00:00:00
-    </div>
-    <div class="card-timer">
-        <ul id="buttons">
-            <li><button onclick="startTimer()">Start</button></li>
-            <li><button onclick="stopTimer()">Stop</button></li>
-
-            <li><button onclick="resetTimer()">Reset</button></li>
-        </ul>
+    <h2>ARS GAMING</h2>
+    <span id='ct5'></span>
+    
+    <div class="card">
+        
+        <div id="stopwatch"> 00:00:00 </div>
+        <table>
+            <tr>
+                <td style="width:100px;">Biaya</td>
+                <td>: </td>
+            </tr>
+            <tr>
+                <td>Jam Mulai</td>
+                <td>: </td>
+            </tr>
+        </table>
+        <div class="contaioner">
+            <ul id="buttons">
+                <li><button onclick="startTimer()">Start</button></li>
+                <li><button onclick="stopTimer()">Stop</button></li>
+                <li><button onclick="resetTimer()">Reset</button></li>
+            </ul>
+        </div>
     </div>
 
     <script src="main.js"></script>
--- main.js
+++ main.js
@@ -53,6 +53,10 @@ function timerCycle() {
 
 function resetTimer() {
     timer.innerHTML = '00:00:00';
+    stoptime = true;
+    hr = 0;
+    sec = 0;
+    min = 0;
 }
 function display_ct5() {
     var x = new Date()
