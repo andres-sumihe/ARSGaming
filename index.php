86787ee002135eaa1e910e6a43e1ab1d4d9e30b9
andres-sumihe <672018136@student.uksw.edu>
2021-04-15 13:35:28 +0900

    Add db file

--- index.php
+++ index.php
@@ -1,32 +1,37 @@
 <?php
-    
+    require ('TxtDb.class.php');
+    $db = new TxtDb([
+        'dir'      => 'db/',
+        'extension' => 'txtdb',
+        'encrypt'   => false,
+    ]);
 ?>
 <html>
-    <head>
-        <title>JavaScript</title>
+<head>
+    <title>ARSGaming</title>
 
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-    <script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<script>
 
-    
-    
-    </script>
-    <span id='ct5'></span>
-        <div id="stopwatch">
-            00:00:00
-        </div>
-        <div class="card-timer">
-            <ul id="buttons">
-                <li><button onclick="startTimer()">Start</button></li>
-                <li><button onclick="stopTimer()">Stop</button></li>
 
-                <li><button onclick="resetTimer()">Reset</button></li>
-            </ul>
-        </div>
 
-        <script src="main.js"></script>
-    </body>
+</script>
+<span id='ct5'></span>
+    <div id="stopwatch">
+        00:00:00
+    </div>
+    <div class="card-timer">
+        <ul id="buttons">
+            <li><button onclick="startTimer()">Start</button></li>
+            <li><button onclick="stopTimer()">Stop</button></li>
+
+            <li><button onclick="resetTimer()">Reset</button></li>
+        </ul>
+    </div>
+
+    <script src="main.js"></script>
+</body>
 </html>
 
